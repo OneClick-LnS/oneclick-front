50985cfbc1ff9bbf69761e2b1ed6434964201295
kim jihwan <kjhwan0802@naver.com>
2024-03-29 18:19:13 +0900

    img drag&drop 완성

--- src/@core/components/DragDrop2.tsx
+++ src/@core/components/DragDrop2.tsx
@@ -1,16 +1,14 @@
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState } from 'react';
 
-// 부모 컴포넌트
 interface DragDropProps {
   onChangeFile: (file: File | null) => void;
   description?: string;
 }
 
-const DragDrop = ({
-  onChangeFile,
-  description = '파일 첨부',
-}: DragDropProps) => {
-  // 사용자가 파일을 드래드 중임을 상태로 관리 UI 변경을 위해 사용
+// Drag & Drop 컴포넌트 : 업로드 된 file 정보를 props로 받은 onChangeFile 함수에 전달
+const DragDrop = ({ onChangeFile }: DragDropProps) => {
+  // 사용자가 파일을 드래그 중임을 상태로 관리
+  // UI 변경을 위해 사용
   const [dragOver, setDragOver] = useState<boolean>(false);
 
   // 드래그 중인 요소가 목표 지점 진입할때
@@ -56,46 +54,27 @@ const DragDrop = ({
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-full">
-      <div className="w-3/4">
-        <label
-          className={`w-full flex-col gap-3 h-32 border-2 ${
-            dragOver
-              ? 'border-blue-500 bg-blue-100 text-blue-500 font-semibold'
-              : 'border-gray-300'
-          } rounded-md flex items-center justify-center cursor-pointer`}
-          htmlFor="fileUpload"
-          // Label에 드래그 앤 드랍 이벤트 추가
-          onDragEnter={handleDragEnter}
-          onDragLeave={handleDragLeave}
-          onDragOver={handleDragOver}
-          onDrop={handleDrop}
-        >
-          {description}
-          <div className="w-9 h-9 pointer-events-none">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1}
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-          </div>
-        </label>
-        <input
-          id="fileUpload"
-          type="file"
-          className=""
-          accept="image/*"
-          onChange={handleChange}
-        ></input>
-      </div>
+    <div className="flex flex-col justify-center items-center w-full h-full absolute z-10">
+      <label
+        className={`w-full h-full flex-col gap-3 border-2 ${
+          dragOver
+            ? 'border-blue-500 bg-blue-100 opacity-50 text-blue-500 font-semibold'
+            : 'border-gray-300'
+        } rounded-md flex items-center justify-center cursor-pointer`}
+        htmlFor="fileUpload"
+        // Label에 드래그 앤 드랍 이벤트 추가
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      ></label>
+      <input
+        id="fileUpload"
+        type="file"
+        className="hidden"
+        accept="image/*"
+        onChange={handleChange}
+      ></input>
     </div>
   );
 };
--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -2,7 +2,7 @@ import removeImg from '@core/assets/removeBackground/remove-background-example.s
 import uploadImg from '@core/assets/removeBackground/background-upload.svg';
 import plusBtn from '@core/assets/removeBackground/plus-btn.svg';
 import { useEffect, useState } from 'react';
-import DragDrop2 from '@core/components/DragDrop2';
+import DragDrop from '@core/components/DragDrop2';
 
 function RemoveBackground() {
   //DragDrop or 직접 버튼 눌러서 업로드 한 파일 상태 저장
@@ -21,55 +21,65 @@ function RemoveBackground() {
     }
   };
 
-  useEffect(() => {}, [file]);
-
   return (
     <>
       <h1 className="text-5xl my-10">Remove Background</h1>
       <p className="my-5">배경을 제거해 보세요!</p>
-      <div className="flex items-center justify-between">
-        <img width={'47%'} src={removeImg} />
-        <div style={{ width: '47%', position: 'relative' }}>
-          {file ? (
-            <>
-              <img width={'100%'} src={URL.createObjectURL(file)} />
+
+      {file ? (
+        <>
+          <div className="flex items-stretch justify-between">
+            <img width={'47%'} src={removeImg} className="rounded-md" />
+            <div
+              style={{
+                width: '47%',
+                position: 'relative',
+                backgroundImage: `url(${URL.createObjectURL(file)})`,
+                backgroundSize: 'contain',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+              }}
+            >
+              <DragDrop onChangeFile={handleFileSelect} />
+            </div>
+          </div>
+
+          <div className="mt-3 flex justify-end items-stretch gap-4">
+            <div className="flex items-center gap-4 bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer">
+              <span>file name : {file.name}</span>
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+              Submit
+            </button>
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex items-stretch justify-between">
+            <img width={'47%'} src={removeImg} className="rounded-md" />
+            <div
+              style={{
+                width: '47%',
+                position: 'relative',
+                backgroundImage: `url(${uploadImg})`,
+              }}
+            >
+              <DragDrop onChangeFile={handleFileSelect} />
+
               <div
                 className={`container flex flex-col items-center`}
                 style={{ position: 'absolute', top: '35%' }}
               >
-                <label className="flex items-center gap-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
-                  <input type="file" className="" accept="image/*"></input>
-                  <img width={'15%'} src={plusBtn} />
-                  <span>select another image</span>
-                </label>
-                <span>Or drop an image here</span>
-              </div>
-              <button>확인</button>
-            </>
-          ) : (
-            <>
-              <img width={'100%'} src={uploadImg} />
-              <div
-                className={`container flex flex-col items-center`}
-                style={{ position: 'absolute', top: '35%' }}
-              >
-                <label className="flex items-center gap-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
-                  <input type="file" className="" accept="image/*"></input>
+                <div className="flex items-center gap-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
                   <img width={'15%'} src={plusBtn} />
                   <span>start from a photo</span>
-                </label>
+                </div>
                 <span>Or drop an image here</span>
               </div>
-            </>
-          )}
-        </div>
-      </div>
-      <DragDrop2 onChangeFile={handleFileSelect} />
-
-      <img
-        src={file ? URL.createObjectURL(file) : ''}
-        className={file ? '' : 'hidden'}
-      />
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
@@ -78,6 +88,22 @@ export default function Home() {
   return (
     <>
       <RemoveBackground />
+    </>
+  );
+}
+
+function test() {
+  return (
+    <>
+      <div className="container flex items-stretch gap-4">
+        <div className="flex items-center gap-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+          <img width={'15%'} src={plusBtn} />
+          <span>select another image</span>
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+          확인
+        </button>
+      </div>
     </>
   );
 }
