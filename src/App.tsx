df35a6a8fbecff46d76225c205334b7d82feb88f
Kim jihwan <kjhwan0802@naver.com>
2024-06-01 22:20:16 +0900

    Router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,37 @@
 // import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Link, BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Gallery from './Gallery';
 import Mypage from './Mypage';
 import Header from '@core/components/Header';
 
-function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: '/home',
-        element: <Home />,
-      },
-      {
-        path: '/about',
-        element: <About />,
-      },
-      {
-        path: '/gallery',
-        element: <Gallery />,
-      },
-      {
-        path: '/mypage',
-        element: <Mypage />,
-      },
-    ],
-    { basename: '/oneclick-front/' },
+function InitPage() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col">
+        <Link to="/home">Home : 이미지 바로 생성하기!</Link>
+        <Link to="/about">About : 사이트 소개!</Link>
+        <Link to="/gallery">Gallery : 생성된 이미지 모음!</Link>
+      </div>
+    </>
   );
+}
+
+function App() {
   return (
     <>
       <div className="mx-10">
-        <Header />
-        <RouterProvider router={router} />
-        {/* <BrowserRouter basename="/oneclick-front/">
+        <BrowserRouter basename="/oneclick-front">
           <Routes>
+            <Route path="/" Component={InitPage} />
             <Route path="/home" Component={Home} />
             <Route path="/about" Component={About} />
             <Route path="/gallery" Component={Gallery} />
             <Route path="/mypage" Component={Mypage} />
           </Routes>
-        </BrowserRouter> */}
+        </BrowserRouter>
       </div>
     </>
   );
